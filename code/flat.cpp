c6526767d8991c573fd73d93164500cced9292d6
Aloysio <aloysiogl@gmail.com>
2020-05-01 15:31:47 -0300

    1.1 flat done

--- code/flat.cpp
+++ code/flat.cpp
@@ -3,4 +3,17 @@
 #include "point.hpp"
 #include "flat.hpp"
 
-// TODO
+flat::flat(cloud *data_, double bandwidth_):radial(data_, bandwidth_) {}
+
+double flat::volume() {
+    double d = (double) point::get_dim();
+    double num = pow(M_PI, d/2);
+    double den = tgamma(d/2 + 1);
+
+    return num/den;
+}
+
+double flat::profile(double t) {
+    if (t < 1) return 1;
+    return 0;
+}
--- code/flat.hpp
+++ code/flat.hpp
@@ -3,4 +3,11 @@
 #include "cloud.hpp"
 #include "radial.hpp"
 
-// TODO
+class flat : public radial
+{
+public:
+    flat(cloud *data_, double bandwidth_);
+
+    double volume();
+    double profile(double t);
+};
